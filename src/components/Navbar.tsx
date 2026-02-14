ca7071b935d3328c098bb14afde50c81949c90ac
Karol Bohdanowicz <karolbohdanowicz@mac.home>
2026-02-14 17:19:06 +0100

    style: Reduce navbar horizontal padding on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,7 +69,7 @@ export default function Navbar() {
                     : "bg-transparent py-8"
             )}
         >
-            <div className="max-w-[1920px] mx-auto px-6 md:px-12 flex items-center justify-between">
+            <div className="max-w-[1920px] mx-auto px-3 md:px-12 flex items-center justify-between">
                 {/* Logo */}
                 <Link href="/" className="relative h-10 w-40 md:h-20 md:w-80 flex items-center z-50 shrink-0">
                     <Image
