f83c54e25603da78d55f50128574c0401089eede
Karol Bohdanowicz <bohunek5@gmail.com>
2026-05-25 21:18:27 +0200

    Update Kisajno amenities and description with IdoBooking 1:1 data

--- src/data/kisajno-data.ts
+++ src/data/kisajno-data.ts
@@ -26,11 +26,14 @@ export const kisajnoData: KisajnoData = {
     title: "Giżycko Kisajno - Apartament z dwoma sypialniami (nr15)",
     price: 500,
     guests: "4",
-    description: `Oferujemy nowoczesny apartament o powierzchni 55 m², położony na parterze z bezpośrednim dostępem do uroków jeziora Kisajno. To oferta premium dla rodzin lub grupy znajomych, które szukają wysokiego standardu, spokojnej lokalizacji i szybkiego dostępu do centrum Giżycka.`,
+    description: `Apartament Kisajno 15A Suite znajduje się na parterze budynku, posiada przestronny salon z aneksem kuchennym i rozkładaną sofę, dwie sypialnie, łazienkę z prysznicem i taras z widokiem na zatokę Tracz i jezioro Kisajno\nSuite z dwoma sypialniami (maksymalna ilość osób 6) Wyposażenie kuchni: ekspres do kawy, płyta indukcyjna, mikrofalówka, piekarnik, lodówka, zmywarka, komplet naczyń i sztućców\nWyposażenie salonu: TV, sofa 2os., stół, krzesła, odkurzacz, klimatyzacja\nWyposażenie sypialni 1: TV, łóżko 160x200, komplet pościeli, suszarka na ubrania, deska do prasowania, żelazko\nWyposażenie sypialni 2: łóżko 160x200, komplet pościeli\nWyposażenie łazienki: prysznic, pralka, suszarka do włosów, ręczniki, żel pod prysznic, mydło, balsam do ciała Wyposażenie tarasu: stolik, krzesełka\nPozostałe: internet, wifi, komplet pościeli i ręczników, zestaw startowy (woda, kawa, herbata).`,
     amenities: {
-        living: ["Klimatyzacja", "Smart TV", "Szybkie WiFi"],
-        kitchen: ["Pełna kuchnia", "Zmywarka"],
-        general: ["Widok na jezioro", "Parking", "Port Neptun"]
+        living: ['TV', 'sofa 2os.', 'stół', 'krzesła', 'odkurzacz', 'klimatyzacja'],
+        kitchen: ['ekspres do kawy', 'płyta indukcyjna', 'mikrofalówka', 'piekarnik', 'lodówka', 'zmywarka', 'komplet naczyń i sztućców'],
+        bedroom: ['TV', 'łóżko 160x200', 'komplet pościeli', 'suszarka na ubrania', 'deska do prasowania', 'żelazko'],
+        bathroom: ['prysznic', 'pralka', 'suszarka do włosów', 'ręczniki', 'żel pod prysznic', 'mydło', 'balsam do ciała'],
+        terrace: ['stolik', 'krzesełka'],
+        general: ['Widok na jezioro', 'Parking', 'Port Neptun', 'Zestaw startowy (woda, kawa, herbata)']
     },
     gallery: {
         heroImage: getAssetPath("/images/kisajno/kisajno_1.webp"),
